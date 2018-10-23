b6c66a9fa27c7fa90b7dd6740eff134d726674c1
antoniocastilla <38734176+antoniocastilla@users.noreply.github.com>
2018-10-23 10:18:11 +0200

    Update pagina10.php

--- MULTIUPLOAD/pagina10.php
+++ MULTIUPLOAD/pagina10.php
@@ -1,13 +1,6 @@
 <?php
 require('classes/Autoload.php');
-/*
-Cuántos archivos venían y cuántos ha podido subir
-Si le pido el nombre me tiene que da los nombres con los que guarda los archivos
-El keep se queda con el prime archivo
-el overwrite con el ultimo
-Y con el rename con todos, por lo que al cambiar el nombre se lo cambio a todos
-El maxSize es solo para uno
-*/
+
 $archivo = new Upload('archivo');
 $archivo->setPolicy(Upload::POLICY_RENAME);
 $r = $archivo->upload();
